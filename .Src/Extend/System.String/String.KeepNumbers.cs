0dee5967ae68e939f6e81c5351b4fd14bd335492
josap <38653194+josap@users.noreply.github.com>
2019-01-17 17:52:13 -0200

    StringEx.KeepNumbersExcept added

--- .Src/Extend/System.String/String.KeepNumbers.cs
+++ .Src/Extend/System.String/String.KeepNumbers.cs
@@ -27,5 +27,24 @@ namespace Extend
                                   .Where( x => x.IsNumber() )
                                   .ToArray() );
         }
+        
+        /// <summary>
+        ///     Removes all characters which aren't numbers with exeptions.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">The string can not be null.</exception>
+        /// <param name="str">The input string.</param>
+        /// <param name="exceptions">The except string. E.g. ".,"</param>
+        /// <returns>A new string containing the exceptions and the numbers of the input string.</returns>
+        [NotNull]
+        [Pure]
+        [PublicAPI]
+        public static String KeepNumbersExcept([NotNull] this String str, [NotNull] string exceptions) 
+        {
+            str.ThrowIfNull(nameof(str));
+
+            return new String(str.ToCharArray()
+                                  .Where(x => x.IsNumber() || x.IsIn(exceptions.ToCharArray()))
+                                  .ToArray());
+        }
     }
 }
